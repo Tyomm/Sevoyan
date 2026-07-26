f67cdf4ccbfd769c2e2a2d78c07897b17b0cc9d4
Tyomm <sevoyanartyom558@gmail.com>
2026-07-26 11:52:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ cartCount.textContent = cart;
 });
 
 });
+function changeLanguage(lang){
+
+const nav = document.querySelectorAll("nav a");
+const heroTitle = document.querySelector(".hero h2");
+const heroText = document.querySelector(".hero p");
+const heroButton = document.querySelector(".hero button");
+
+if(lang==="hy"){
+
+nav[0].textContent="Գլխավոր";
+nav[1].textContent="Խանութ";
+nav[2].textContent="Հավաքածուներ";
+nav[3].textContent="Մեր մասին";
+nav[4].textContent="Կապ";
+
+heroTitle.textContent="ՆՈՐ ՀԱՎԱՔԱԾՈՒ";
+heroText.textContent="Հագի՛ր Քո Վստահությունը";
+heroButton.textContent="Գնել հիմա";
+
+}else{
+
+nav[0].textContent="Home";
+nav[1].textContent="Shop";
+nav[2].textContent="Collections";
+nav[3].textContent="About";
+nav[4].textContent="Contact";
+
+heroTitle.textContent="NEW COLLECTION";
+heroText.textContent="Wear Your Confidence";
+heroButton.textContent="Shop Now";
+
+}
+
+}
